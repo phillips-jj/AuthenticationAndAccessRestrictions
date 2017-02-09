faad97e7174b3c67a84922d5e55d4ea19b19aae3
phillips-jj <jjphillips@hotmail.com>
2017-02-09 11:05:12 -0500

    Clarified comments

--- test-login.py
+++ test-login.py
@@ -4,6 +4,10 @@ import string
 
 from auth import AuthController, require, member_of, name_is
 
+# Customized version of the AuthenticationAndAccessRestrictions sample from http://tools.cherrypy.org/wiki/AuthenticationAndAccessRestrictions.
+# I've added some Ajax/Javascript/REST calls based on the CherryPy tutorial, and also slightly enhanced some of the sample web pages.  Check out
+# my WebAutomationCSharp sample to see how we can use NHTML to automate testing of this web UI.
+
 class RestrictedArea:
     
     # all methods in this controller (and subcontrollers) is
